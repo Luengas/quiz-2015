f13d2af42197f0e860b21c03880adf015a8d8fab
Iñaki Luengas <luengas33@hotmail.com>
2015-06-19 17:31:16 +0200

    Validación
    Validación

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -13,14 +13,24 @@ exports.load = function(req, res, next, quizId) {
 };
 
 
+
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(
+	var _patronBusqueda = req.query.search || "";
+	 _patronBusqueda = "%" + _patronBusqueda.replace(/\s/gi, "%") + "%";
+	 // Objeto que modela una pseudo clausula WHERE de SQL
+	var _paramBusqueda = {
+    // El comodin "?" de la expresión de la primera posición
+    // del array se sustituye por el contenido de la segunda posición
+    where: ["pregunta like ?", _patronBusqueda]};
+  
+  models.Quiz.findAll(_paramBusqueda).then(
     function(quizes) {
       res.render('quizes/index.ejs', {quizes: quizes, errors: []});
     }
-  ).catch(function(error){next(error)});
+  ).catch(function(error) { next(error);})
 };
+
 
 // GET /quizes/:id
 exports.show = function(req, res) {
