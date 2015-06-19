f49cfb8937f2ebf6fb858e2a7b210ea4e9546e94
Iñaki Luengas <luengas33@hotmail.com>
2015-06-19 19:12:25 +0200

    Borrar pregunta
    Borrar pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -105,6 +105,15 @@ exports.update = function(req, res) {
 };
 
 
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+  req.quiz.destroy().then( function() {
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
+};
+
+//  console.log("req.quiz.id: " + req.quiz.id);
+
 
 // GET /author
 exports.author = function(req, res) {
