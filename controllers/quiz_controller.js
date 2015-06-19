0c14690b61c726f1a156e1edb7bf6c04eafe9c23
Iñaki Luengas <luengas33@hotmail.com>
2015-06-19 16:33:10 +0200

    Crear pregunta
    Crear pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -48,3 +48,22 @@ exports.author = function(req, res) {
    res.render('author', {autor: 'Iñaki Luengas'});
 };
 
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build(
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+
+  res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+
+// guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');  
+  })   // res.redirect: Redirección HTTP a lista de preguntas
+};
+
