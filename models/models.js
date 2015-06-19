eb697681e96afcfdcc759c7c85ff838d7859df1f
Iñaki Luengas <luengas33@hotmail.com>
2015-06-20 01:43:34 +0200

    Pruebas
    Pruebas

--- models/models.js
+++ models/models.js
@@ -39,9 +39,9 @@ sequelize.sync().then(function() {
     if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.bulkCreate( 
         [ {pregunta: 'Capital de Italia',   respuesta: 'Roma',
-          tema: "Humanidades"},
+          tema: 'Humanidades'},
           {pregunta: 'Capital de Portugal', respuesta: 'Lisboa',
-          tema: "Humanidades"}
+          tema: 'Humanidades'}
         ]
       ).then(function(){console.log('Base de datos inicializada')});
     };
