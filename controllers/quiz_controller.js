7622fe4fb5520b03485ddd6e0d09c6e032ad72fc
Iñaki Luengas <luengas33@hotmail.com>
2015-06-19 16:52:47 +0200

    Validación
    Validación

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -67,3 +67,22 @@ exports.create = function(req, res) {
   })   // res.redirect: Redirección HTTP a lista de preguntas
 };
 
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')}) 
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  );
+};
+
