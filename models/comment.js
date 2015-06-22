a1d5145602aee93f48f4b42fe3815669ba69fe5e
Iñaki Luengas <luengas33@hotmail.com>
2015-06-22 23:09:25 +0200

    Modulo 9 - Ejercicio opcional
    Modulo 9 - Ejercicio opcional

--- models/comment.js
+++ models/comment.js
@@ -17,7 +17,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       countUnpublished: function () {
         //Comentario no publicados
-		return this.count({where: {publicado: 'false'}}).on('success', function(count) {
+		return this.count({where: {publicado: 'false'}}).then('success', function(count) {
 			return count;
     })
       },
