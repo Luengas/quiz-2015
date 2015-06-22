5b32bc129e0c530c9364fe56332919a37e4528d2
Iñaki Luengas <luengas33@hotmail.com>
2015-06-22 22:42:38 +0200

    Modulo 9 - Ejercicio opcional
    Modulo 9 - Ejercicio opcional

--- models/comment.js
+++ models/comment.js
@@ -12,15 +12,16 @@ module.exports = function(sequelize, DataTypes) {
       	defaultValue: false
       }
     }    
- ,
-  {
-    classMethods: {
-      countUnpublished: function () {
-        return 10;
-      },
-      countCommentedQuizes: function () {
-        return 20;
-      }
-    }
-  });
+ //,
+  //{
+  //  classMethods: {
+  //    countUnpublished: function () {
+  //      return 10;
+  //    },
+  //    countCommentedQuizes: function () {
+   //     return 20;
+    //  }
+   // }
+  //}
+  );
 };
